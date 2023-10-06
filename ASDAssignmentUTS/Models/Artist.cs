cf859b1d06eb3a9683e9fd9c38a66cbe5f45316c
rowanlilley <rowan.lilley@gmail.com>
2023-10-06 15:01:26 +1100

    Added DBConnector

--- ASDAssignmentUTS/Models/Artist.cs
+++ ASDAssignmentUTS/Models/Artist.cs
@@ -4,41 +4,41 @@ using System.Data.SqlClient;
 
 namespace ASDAssignmentUTS.Models;
 
-    public class Artist
-    {
-        public int id { get; set; }
-        public string name { get; set; }
-        public string genre { get; set; }
-        public string country { get; set; }
-        public string description { get; set; }
+public class Artist
+{
+    public int id { get; set; }
+    public string name { get; set; }
+    public string genre { get; set; }
+    public string country { get; set; }
+    public string description { get; set; }
 
-            public List<Artist> GetArtists()
+    public List<Artist> GetArtists()
+    {
+        string connectionString = DBConnector.GetConnectionString();
+        List<Artist> artists = new List<Artist>();
+        using (var connection = new SqlConnection(connectionString))
+        {
+            using (var command = new SqlCommand())
             {
-                string connectionString = "Data Source=asdassignmentuts.database.windows.net;Initial Catalog=SongManagementSystem;Persist Security Info=True;User ID=azureuser;Password=ASDAssignment1";
-                List<Artist> artists = new List<Artist>();
-                using (var connection = new SqlConnection(connectionString))
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Artist";
+                var reader = command.ExecuteReader();
+                while (reader.Read())
                 {
-                    using (var command = new SqlCommand())
+                    Artist artist = new Artist()
                     {
-                        connection.Open();
-                        command.Connection = connection;
-                        command.CommandText = "SELECT * FROM Artist";
-                        var reader = command.ExecuteReader();
-                        while (reader.Read())
-                        {
-                            Artist artist = new Artist()
-                            {
-                                id = reader.GetInt32(0),
-                                name = reader.GetString(1),
-                                genre = reader.GetString(2),
-                                country = reader.GetString(3),
-                                description = reader.GetString(4)
-                            };
-                            artists.Add(artist);
-                        }
-                    }
+                        id = reader.GetInt32(0),
+                        name = reader.GetString(1),
+                        genre = reader.GetString(2),
+                        country = reader.GetString(3),
+                        description = reader.GetString(4)
+                    };
+                    artists.Add(artist);
                 }
-                return artists;
-           
             }
         }
+        return artists;
+
+    }
+}
