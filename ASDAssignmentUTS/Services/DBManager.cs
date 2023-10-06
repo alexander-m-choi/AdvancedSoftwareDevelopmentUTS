d3ab15f8049eccb34c55f32d463df5c80ecc4e40
choc0099 <choclate00@live.com>
2023-10-06 19:57:10 +1100

    Created a DatabaseManager class that will be used for my features.

---
+++ ASDAssignmentUTS/Services/DBManager.cs
@@ -0,0 +1,26 @@
+﻿using ASDAssignmentUTS.Models;
+using System.Data.SqlClient;
+
+namespace ASDAssignmentUTS.Services
+{
+    //this is a helper class i am going to use for my features.
+    public static class DBManager
+    {
+        static readonly string connectionStr = DBConnector.GetConnectionString();
+
+        static public void AddArtist(Artist artist)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionStr))
+            {
+                conn.Open();
+                string sql = @"INSERT INTO Artist (name, genre, country, description) VALUES (@name, @genre, @country, @description)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@name", artist.name);
+                cmd.Parameters.AddWithValue("@genre", artist.genre);
+                cmd.Parameters.AddWithValue("@country", artist.country);
+                cmd.Parameters.AddWithValue("@description", artist.description);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
