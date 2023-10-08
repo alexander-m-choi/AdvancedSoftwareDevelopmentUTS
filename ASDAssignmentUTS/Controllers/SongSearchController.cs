7ec81f4f0dfbaeb06fbe4d5fd09f17874810518d
AamashQaisar <mohammad.a.qaisar@student.uts.edu.au>
2023-10-08 22:03:28 +1100

    View update

--- ASDAssignmentUTS/Controllers/SongSearchController.cs
+++ ASDAssignmentUTS/Controllers/SongSearchController.cs
@@ -1,15 +1,33 @@
 ﻿using ASDAssignmentUTS.Models;
 using ASDAssignmentUTS.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace ASDAssignmentUTS.Controllers
 {
-    public class SongSearchController
+    public class SongSearchController : Controller
     {
-        public ActionResult SongManagement()
+
+        public ActionResult SongSearch()
         {
-            List<Song> songs = SongDBManager.GetSongs(string songname);
+            List<Song> songs = SongDBManager.GetSongs();
             return View(songs);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult GetSongsByName(IFormCollection collection)
+        {
+            if (collection["name"] != "")
+            {
+                var song = new Song();
+                //captures the data from the form that was imputed by the user.
+                string name = collection["name"];
+                List<Song> songs = SongDBManager.GetSongsByName(name);
+                return View(songs);
+            }
+            return RedirectToAction(nameof(SongSearch));
         }
     }
 }
