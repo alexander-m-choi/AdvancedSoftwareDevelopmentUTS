b0ed20ddc5d5a7935cf0bf08a00d8fd6033ec60d
AamashQaisar <mohammad.a.qaisar@student.uts.edu.au>
2023-10-10 21:54:22 +1100

    Unit Testing Implemented for Song Search

---
+++ SongSearchTest/SongDBTest.cs
@@ -0,0 +1,31 @@
+using ASDAssignmentUTS;
+using ASDAssignmentUTS.Controllers;
+using ASDAssignmentUTS.Models;
+using ASDAssignmentUTS.Services;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SongSearchTest
+{
+    [TestClass]
+    public class SongDBTest
+    {
+        [TestMethod]
+        public void TestSongSearch()
+        {
+            Song song = new Song();
+            song.name = "Fire";
+            song.artistId = 1;
+            song.genre = "Rock";
+            song.description = "The latest music from Imagine Vampires.";
+            //adds it directly to the database
+            SongDBManager.AddSong(song);
+            Song testsong = SongDBManager.GetSongByName("Fire");
+            Assert.AreEqual("Fire", song.name);
+            Assert.AreEqual("Rock", song.genre);
+            Assert.AreEqual("The latest music from Imagine Vampires.", song.description);
+        }
+    }
+}
