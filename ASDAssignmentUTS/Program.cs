1c6d6a44594b7a3cfddcab13acbbbca9e732642a
rowanlilley <rowan.lilley@gmail.com>
2023-11-02 18:05:24 +1100

    Added Playlist Functionalities

--- ASDAssignmentUTS/Program.cs
+++ ASDAssignmentUTS/Program.cs
@@ -1,9 +1,11 @@
 using ASDAssignmentUTS.Repositories;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Session;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSession();
 
 builder.Services.AddScoped<UserRepository>(serviceProvider =>
     new UserRepository(DBConnector.GetConnectionString()));
@@ -35,6 +37,8 @@ app.UseRouting();
 app.UseCookiePolicy();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
